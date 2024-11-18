4d8676c4d76de8f72311daa128822c5b226fea29
Tushar <tusharvarshney1810@gmail.com>
2024-11-18 16:41:27 +0530

    removed cors conffig

--- index.js
+++ index.js
@@ -10,17 +10,17 @@ dotenv.config(); // Load environment variables
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const corsOptions = {
-  origin: "*",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
+// const corsOptions = {
+//   origin: "*",
+//   credentials: true, //access-control-allow-credentials:true
+//   optionSuccessStatus: 200,
+// };
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json()); // Built-in JSON parser in Express
 app.use(cookieParser());
-app.use(cors(corsOptions));
+app.use(cors());
 
 // Routes
 app.use("/api/todos", todoRoutes);
