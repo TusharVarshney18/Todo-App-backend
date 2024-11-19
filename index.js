af6599259e6c75aa1fb06f2f3edd4fec9bb9486a
Tushar <tusharvarshney1810@gmail.com>
2024-11-19 12:22:59 +0530

    Fixed index.js , vercel.js , and add helmet

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
-const mongoose = require("mongoose");
 const express = require("express");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const todoRoutes = require("./Routes/todos.js");
 const cookieParser = require("cookie-parser");
+const helmet = require("helmet");
 
 dotenv.config(); // Load environment variables
 
 const app = express();
-const PORT = "https://todo-app-backend-git-main-tushar-varshneys-projects.vercel.app" || 3000;
+const PORT = process.env.PORT || 3000;
 
-
+// Configure Helmet for Security
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "https://vercel.live"],
+      },
+    },
+    crossOriginEmbedderPolicy: false, // Disable if you're using cross-origin resources
+  })
+);
 
 // Middleware
 app.use(express.json()); // Parse incoming JSON
 app.use(express.urlencoded({ extended: false })); // Parse URL-encoded data
 app.use(cookieParser()); // Parse cookies
 
-
 // Routes
-
-app.use("/", require('./Routes/Auth.route.js'))
+app.use("/", require("./Routes/Auth.route.js"));
 app.use("/api/todos", todoRoutes);
-
-
-
 
 // MongoDB connection
 mongoose
