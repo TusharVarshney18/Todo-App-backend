f367ca8e08a4a99186e9166dcafb27231938eb5f
Tushar <tusharvarshney1810@gmail.com>
2024-11-20 16:42:02 +0530

    fixed Route error

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(express.urlencoded({ extended: false })); // Parse URL-encoded data
 app.use(cookieParser()); // Parse cookies
 
 // Routes
-app.use("/", require("./Routes/Auth.route.js")); // Authentication routes (unchanged)
+app.use("/", require("./Routes/Auth.route.js"));
 app.use("/api/todos", todoRoutes); // Todo routes
 
 // MongoDB Connection
@@ -53,10 +53,7 @@ app.get("/", (req, res) => {
   res.json({ message: "API is working!" });
 });
 
-// Error Handling for Unmatched Routes
-app.use((req, res, next) => {
-  res.status(404).json({ error: "Route not found" });
-});
+
 
 // Global Error Handler
 app.use((err, req, res, next) => {
